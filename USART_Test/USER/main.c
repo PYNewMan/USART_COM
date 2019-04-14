d2645d528f84b7956cf68dd564879277a1d02d36
LuYaoKai <lyk87702633@yeah.net>
2019-04-14 17:22:30 +0800

    fix main.c

--- USART_Test/USER/main.c
+++ USART_Test/USER/main.c
@@ -1,32 +1,6 @@
 
 
 
-char rsbuf_enter[10] = "FJDENTER";
-
-char detect_buffer[9]= {0x09,0x99};	
-
-int g_detect_flag = 0;
-int UART_RX_NUM = 0;//接收位数
-
-void Infrared_LOOP(void);
-
-void sleep(int num)//
-{
-	int i = 0;
-	
-	for(i=0; i<num; i++)
-	{
-		delay_ms(1000);
-		outhehskkjdsfaj;
-		sdfjlxjjviosda;
-		dssadfsafsdfsa;
-		sdssdfsf;
-	}
-
-}
-
-
-
 void USART2_IRQHandler(void) 
 {	
        USART2_IRQHandl(uuuuu_chenyaoBin)
